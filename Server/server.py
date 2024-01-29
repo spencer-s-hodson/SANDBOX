882e70ad2578b413be398a2bb19cc374d8b5537a
Isaac Lloyd <isaacwlloyd@gmail.com>
2024-01-29 08:55:09 -0700

    final commit of hackathon

--- Server/server.py
+++ Server/server.py
@@ -7,17 +7,18 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})  # Allow all origins for any route
 
 
-# db = MySQLDatabase('localhost', 'mydatabase', 'username', 'password', 'mytable')
+db = MySQLDatabase('localhost', 'hackathon', 'kerdos', 'kerdos123', 'listings')
 
 @app.route('/get-rows', methods=['GET'])
 def get_rows():
     address = request.args.get('address')
 
     # DUMMY DATA
-    l: list = [
-        ('john', 'brown', '892 e 700 n provo ut 84604', '800', 'covered', '949-709-9241'),
-        ('mike', 'white', '1515 N 300 W st provo ut 84604', '800', 'uncovered', '949-632-6104'),
-    ]
+    #l: list = [
+    #    ('john', 'brown', '892 e 700 n provo ut 84604', '800', 'covered', '949-709-9241'),
+    #    ('mike', 'white', '1515 N 300 W st provo ut 84604', '800', 'uncovered', '949-632-6104'),
+    #]
+    l = db.query_data()
 
     # Placeholder response for testing
     return jsonify(l)
@@ -29,7 +30,7 @@ def get_rows():
 def insert_row():
     data = request.get_json()
     print(data)
-    # db.insert_data(data)
+    db.insert_data(data)
     return jsonify({"message": "Row inserted successfully"})
 
 if __name__ == '__main__':
--- Server/test.py
+++ Server/test.py
@@ -1,18 +1,11 @@
 from database import MySQLDatabase
 
-insert = {'First_Name' : 'john', 'Last_Name' : 'john', 'Address' : 'wrong address1', 'Price' : 1243.44, 'Type' : 'Covered', 'Contact'  : "898-999-9999"}
-insert1 = {'First_Name' : 'mary', 'Last_Name' : 'john', 'Address' : 'ASDSA', 'Price' : 1243.44, 'Type' : 'Covered', 'Contact'  : "898-999-9999"}
-insert2 = {'First_Name' : 'alex', 'Last_Name' : 'john', 'Address' : 'adsfadsf', 'Price' : 1243.44, 'Type' : 'Covered', 'Contact'  : "898-999-9999"}
-insert3 = {'First_Name' : 'bodsflkjh;dflsb', 'Last_Name' : 'john', 'Address' : 'dasfdsfa', 'Price' : 1243.44, 'Type' : 'Covered', 'Contact'  : "898-999-9999"}
-insert4 = {'First_Name' : 'bodsflkjh;dflsb', 'Last_Name' : 'john', 'Address' : 'asfdsfad', 'Price' : 1243.44, 'Type' : 'Covered', 'Contact'  : "898-999-9999"}
-
+insert = {'First_Name' : 'johnhy', 'Last_Name' : 'john', 'Address' : 'bad address', 'Price' : 1243.44, 'Type' : 'Covered', 'Contact'  : "898-999-9999"}
+ 
 db = MySQLDatabase('localhost', 'hackathon', 'kerdos', 'kerdos123', 'listings')
 
 db.insert_data(insert)
-db.insert_data(insert1)
-db.insert_data(insert2)
-db.insert_data(insert3)
-db.insert_data(insert4)
+
 print(db.query_data())
 print()
 print(db.query_data_sorted("Brigham Young University, Provo, UT 84602, United States", 2))
